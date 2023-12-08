d3ec058a81a03ebbc64f5eb317bba58c2f56f931
Anna Vigolo <vigoloanna1998@gmail.com>
2023-12-08 13:38:34 +0100

    ciaoprova1

--- convoluzione2.py
+++ convoluzione2.py
@@ -6,7 +6,7 @@ from scipy import integrate
 from scipy import optimize as opt
 import scipy
 import ROOT
-#non ciao,meglio salve
+#ciao prova1
 
 from iminuit import Minuit,cost
 import uncertainties
